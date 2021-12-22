49e2726d2fbfa177aa4bdf91e894f892153b3b14
William Cantin <william.cantin@broadsign.com>
2021-12-22 17:09:42 -0500

    Init frontend with a basic backend endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useEffect, useState} from "react";
+import css from './App.css';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [screens, setScreens] = useState([]);
+  useEffect(() => {
+      fetch("/screens")
+          .then(res => res.json())
+          .then(
+              (result) => {
+                  setScreens(result.screens);
+              },
+              (error) => {
+                  console.error(error)
+              }
+          )
+  }, [])
+    return (
+        <ul>
+            {screens ? screens.map((s, index) => (
+                <li key={index}>
+                    {s.name}
+                </li>
+            )) : 'loading'}
+        </ul>)
 }
 
 export default App;
